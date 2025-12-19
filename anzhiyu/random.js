758bdfb732de2a6ed0e8fb8ad007136463c414cd
gedongyou <gedongyou@qq.com>
2025-12-19 21:58:33 +0800

    Site updated: 2025-12-19 21:58:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/eu-org域名申请教程/","posts/hexo的搭建/","posts/安装-Dell-OMSA/","posts/玩转console.log/"];function toRandomPost(){
+var posts=["posts/eu-org域名申请教程/","posts/安装-Dell-OMSA/","posts/hexo的搭建/","posts/玩转console.log/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
